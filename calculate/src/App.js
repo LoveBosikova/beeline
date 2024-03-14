80d94e5603915e7ec0fc848762e5f79113cb86a4
Bosikova Liubov Sergeevna <gulidova06@mail.ru>
2024-03-15 00:47:14 +0300

    header, footer

--- calculate/src/App.js
+++ calculate/src/App.js
@@ -4,11 +4,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header></header>
+      <header className='header__wrap'>
+        <h1 className='header__text'>Калькулятор предложения</h1>
+      </header>
       <main>
-        
+
       </main>
-      <footer></footer>
+      <footer className='footer'>
+        <h3 className='footer__h3'>Нашли ошибку или не хватает функционала?</h3>
+        <p className='footer__text'> Пишите нам на <a href='https://t.me/TheZavitaev' className='footer__link'>@TheZavitaev</a>, мы всё быстро починим!</p>
+      </footer>
     </div>
   );
 }

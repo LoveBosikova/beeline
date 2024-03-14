25631dc94996a26f8c4ae590fd2158e23d3288f3
Bosikova Liubov Sergeevna <gulidova06@mail.ru>
2024-03-14 23:54:22 +0300

    start app plus fonts

---
+++ calculate/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header></header>
+      <main>
+        
+      </main>
+      <footer></footer>
+    </div>
+  );
+}
+
+export default App;

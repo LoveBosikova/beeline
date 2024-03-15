b018ffdc5f6d6f3c12db3d8c02d1bc30654f29cd
Bosikova Liubov Sergeevna <gulidova06@mail.ru>
2024-03-15 11:47:00 +0300

    tabs

--- calculate/src/App.js
+++ calculate/src/App.js
@@ -1,13 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { useState } from 'react';
+
+import Form from 'react-bootstrap/Form';
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
 
 function App() {
+  const [key, setKey] = useState('workspace');
   return (
-    <div className="App">
+    <div className='App' data-bs-theme='dark'>
       <header className='header__wrap'>
         <h1 className='header__text'>Калькулятор предложения</h1>
       </header>
       <main>
+        <Form className='form'>
+          <Tabs
+            id='options'
+            activeKey={key}
+            onSelect={(k) => setKey(k)}
+            className="mb-3"
+            >
+            <Tab eventKey='workspace' title='DF Workspace Premium'>
+              <p className='tabText'>Tab content for DF Workspace Premium</p>
+            </Tab>
+            <Tab eventKey='backspace' title='Хранилище резервных копий'>
+              Tab content for Хранилище резервных копий
+            </Tab>
+            <Tab eventKey='veeam' title='Резервное копирование Veeam'>
+              Tab content for Резервное копирование Veeam
+            </Tab>
+          </Tabs>
+        </Form>
 
       </main>
       <footer className='footer'>
